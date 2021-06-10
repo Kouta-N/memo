0e7e6871560ecc1976c10c1f54525e3e2e9f64f4
kakashi <nk29time@gmail.com>
2021-06-10 15:28:27 +0900

    second

--- dbconnnect.php
+++ dbconnnect.php
@@ -2,6 +2,6 @@
 try {
     $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8', 'root', 'root');
 } catch (PDOException $e) { 
-    echo 'DB接続エラー:　' . $e->getMessage();
+    echo 'DB接続エラー: ' . $e->getMessage();
 }
 ?>
